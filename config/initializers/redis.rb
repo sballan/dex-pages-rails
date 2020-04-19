209d5181c039fcf672a3dde9000dcff80f1a5a53
Samuel Ballan <sgb4622@gmail.com>
2020-04-18 20:50:46 -0400

    redis pages instance

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -8,4 +8,7 @@ Redis::Objects.redis = $redis_objects
 redis_words_url = ENV['REDIS_WORDS_URL'] || ENV['REDIS_URL'] || 'redis://localhost'
 $redis_words = Redis.new(url: redis_words_url)
 
+redis_pages_url = ENV['REDIS_PAGES_URL'] || ENV['REDIS_URL'] || 'redis://localhost'
+$redis_pages = Redis.new(url: redis_pages_url)
+
 
