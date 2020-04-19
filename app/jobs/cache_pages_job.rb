7034892c0a66eb6600ad5983c4d2db2872f590a8
Samuel Ballan <sgb4622@gmail.com>
2020-04-18 21:08:02 -0400

    typo

--- app/jobs/cache_pages_job.rb
+++ app/jobs/cache_pages_job.rb
@@ -1,7 +1,7 @@
 class CachePagesJob < ApplicationJob
   def perform
     Page.only_with_page_file.in_batches.each_record do |page|
-      CachePagesJob.perform_later(page)
+      CachePageJob.perform_later(page)
     end
   end
 end
