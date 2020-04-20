3088270bc098141cdfea8dc90309af3a798a9876
Samuel Ballan <sgb4622@gmail.com>
2020-04-19 21:19:26 -0400

    updates to download links, crawler will not use uri query or fragment

--- app/jobs/download_links_job.rb
+++ app/jobs/download_links_job.rb
@@ -5,17 +5,32 @@ class DownloadLinksJob < ApplicationJob
   # Most jobs are safe to ignore if the underlying records are no longer available
   discard_on ActiveJob::DeserializationError
 
-  def perform(page, should_extract_words = true)
+  # NOTE: This job currently will remove the query and fragment portion of urls.
+  def perform(page)
     page = Page.find page unless page.is_a?(Page)
 
     page.download_page_file unless page.cached_page_file
 
-    page.reload.links.each do |link|
-      link_page = Page.create_or_find_by!(url: link)
-      link_page.download_page_file unless link_page.cached_page_file
-      ExtractWordsJob.perform_later(link_page) if should_extract_words
-    rescue Page::FetchInvalidError
+    # TODO: make this configurable. For now, we remove any url params and url fragment
+    links_to_download = page.reload.links.map do |link|
+      uri = URI(link)
+      uri.query = nil
+      uri.fragment = nil
+      uri.to_s
     end
+
+    links_to_download.each do |link|
+      process_link(link)
+    end
+  end
+
+  def process_link(link_string)
+    link_page = Page.create_or_find_by!(url: link_string)
+    link_page.download_page_file unless link_page.cached_page_file
+
+    ExtractWordsJob.perform_later(link_page)
+    CachePageJob.perform_later(link_page)
+  rescue Page::FetchInvalidError
   end
 
 end
